cf16d848dec9fb7bb7d9319596e2227b0ce2312e
kleyr567 <claireannebohol567@gmail.com>
2024-11-21 09:55:31 +0800

    fix: remove comments

--- src/views/auth/Advertise.vue
+++ src/views/auth/Advertise.vue
@@ -6,13 +6,11 @@ export default {
       showLoading: false,
       showGifs: false,
       gifs: [
-<<<<<<< HEAD
+
         '/src/images/advertise1.webp', 
         'src/images/advertise.gif',
-=======
         '/src/images/gif1.gif', 
         'https://cdn.prod.website-files.com/652c101aadcd91f29004ee59/65aec32e1ca1bbfc25eee730_KP9-studio-main.gif',
->>>>>>> development
       ],
       currentGifIndex: 0,
       displayText: false,
